d945fe8a08a9df60679d55eb8b2beb2e7443996e
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2025-02-22 10:46:43 -0500

    Initial commit: Set up Vite React project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import Calendar from './components/Calendar';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My Timetable Project</h1>
+      <h2>A special schedule for [Your Recipient or Theme]</h2>
+
+      {/* Insert the Calendar component here */}
+      <Calendar />
+    </div>
+  );
+}
+
+export default App;
