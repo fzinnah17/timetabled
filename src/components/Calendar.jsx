6b633daedc41c122a8eb3c1785b1a4754de6b851
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2025-02-22 10:48:41 -0500

    Replace test message with a grid-based weekly calendar in Calendar.jsx

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,11 +1,148 @@
 import React from "react";
 
+import React from "react";
+
 const Calendar = () => {
   return (
     <div className="Calendar">
-      Testing the calendar component
+      <table>
+        <thead>
+          <tr>
+            <th></th>
+            <th>Sunday</th>
+            <th>Monday</th>
+            <th>Tuesday</th>
+            <th>Wednesday</th>
+            <th>Thursday</th>
+            <th>Friday</th>
+            <th>Saturday</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* 8 am row */}
+          <tr>
+            <td className="time">8 am</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+
+          {/* 9 am row */}
+          <tr>
+            <td className="time">9 am</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+
+          {/* 10 am row */}
+          <tr>
+            <td className="time">10 am</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+
+          {/* 11 am row */}
+          <tr>
+            <td className="time">11 am</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+
+          {/* 12 pm row */}
+          <tr>
+            <td className="time">12 pm</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+
+          {/* 1 pm row */}
+          <tr>
+            <td className="time">1 pm</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+
+          {/* 2 pm row */}
+          <tr>
+            <td className="time">2 pm</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+
+          {/* 3 pm row */}
+          <tr>
+            <td className="time">3 pm</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+
+          {/* 4 pm row */}
+          <tr>
+            <td className="time">4 pm</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+
+          {/* 5 pm row */}
+          <tr>
+            <td className="time">5 pm</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
 
 export default Calendar;
+
