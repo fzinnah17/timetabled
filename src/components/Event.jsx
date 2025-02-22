86c7cc07eac9dc56274914285acadd4ed023f291
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2025-02-22 10:50:12 -0500

    Modify Calendar component to pass props to Event component

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -1,11 +1,44 @@
 import React from "react";
 
-const Event = (props) => {
-  return (
-    <td className="Event">
-      <h5>Test Event Name</h5>
-    </td>
-  );
-};
+import React from "react";
+import Event from "./Event";   // <-- import
 
-export default Event;
+const Calendar = () => {
+  return (
+    <div className="Calendar">
+      <table>
+        <thead>
+          <tr>
+            <th></th>
+            <th>Sunday</th>
+            <th>Monday</th>
+            <th>Tuesday</th>
+            <th>Wednesday</th>
+            <th>Thursday</th>
+            <th>Friday</th>
+            <th>Saturday</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* 8 am row */}
+          <tr>
+            <td className="time">8 am</td>
+            {/* Use <Event /> in place of one <td> */}
+            <Event />
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+
+          {/* ... more rows here */}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default Calendar;
+
